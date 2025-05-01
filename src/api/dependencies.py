10c52ad8ff6c79d4bc0adb36981c808380f97e86
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-05-01 22:49:49 +0300

    Update dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,6 +1,14 @@
 from fastapi import Depends
 from database import get_db
 from src.repositories.user import UserRepository
+from src.repositories.progress import ProgressRepository
+from src.repositories.achievement import AchievementRepository
 
 async def get_user_repository(session=Depends(get_db)) -> UserRepository:
     return UserRepository(session)
+
+async def get_progress_repository(session=Depends(get_db)) -> ProgressRepository:
+    return ProgressRepository(session)
+
+async def get_achievement_repository(session=Depends(get_db)) -> AchievementRepository:
+    return AchievementRepository(session)
