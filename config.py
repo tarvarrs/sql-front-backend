96d96c4c6c7433e021dfbe2dc9cc0d21fb655fba
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-05-04 22:39:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DB_URL")
+    GAME_DATABASE_URL: str = os.getenv("GAME_DATABASE_URL")
     SECRET_KEY: str = os.getenv("ACCESS_TOKEN_SECRET")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
