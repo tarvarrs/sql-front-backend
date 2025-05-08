bf10cc1bd5757be8d34047ae933706b0e92ec08c
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-05-08 12:51:59 +0300

    add clues system

--- src/repositories/task.py
+++ src/repositories/task.py
@@ -1,13 +1,16 @@
 from fastapi import HTTPException
-from sqlalchemy import select, func, outerjoin, exists, update, and_
+from sqlalchemy import select, func, outerjoin, exists, update, and_, delete
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.repositories.user import UserRepository
 from src.models.progress import UserProgress
 from src.models.user import User
 from src.models.task import Task, TaskSolved
 from src.repositories.achievement import AchievementRepository
+from src.models.clue import PurchasedClue
 from typing import Dict, List
 from collections import defaultdict
 from config import settings
+
 
 class TaskRepository:
     def __init__(self, session: AsyncSession):
@@ -199,7 +202,7 @@ class TaskRepository:
         )
         task_global_id = task.scalar()
         if not task_global_id:
-            raise HTTPException(status_code=404, detail="Task not found")
+            raise HTTPException(status_code=404, detail="Задача не найдена")
 
         already_solved = await self.session.execute(
             select(exists().where(
@@ -236,6 +239,7 @@ class TaskRepository:
                     .values(**{progress_field: getattr(UserProgress, progress_field) + 1})
                 )
                 message = f"Правильно! Заработано {points} баллов"
+                await self.clear_purchased_clues(user_id, task_global_id)
             else:
                 message = f"Правильно! За повторное решение баллы не начисляются"
         else:
@@ -270,10 +274,48 @@ class TaskRepository:
         
         await self.session.commit()
 
+        user_repo = UserRepository(self.session)
+        user_progress = await user_repo.get_user_progress_by_id(user_id)
+
         return {
             "was_solved_before": already_solved,
             "points_earned": points_earned,
             "points_penalty": points_penalty,
             "message": message,
-            "awarded_achievements": awarded_achievements
+            "awarded_achievements": awarded_achievements,
+            "current_points": user_progress.total_score
         }
+    async def purchase_clue(
+            self,
+            user_id: int,
+            task_global_id: int,
+            clue_type: int,
+            cost: int
+    ) -> bool:
+        user = await self.session.get(User, user_id)
+        if user.total_score < cost:
+            raise HTTPException(
+                status_code=400,
+                detail="Недостаточно баллов для покупки подсказки"
+            )
+        user.total_score -= cost
+
+        self.session.add(PurchasedClue(
+            user_id=user_id,
+            task_global_id=task_global_id,
+            clue_type=clue_type
+        ))
+        await self.session.commit()
+        return True
+
+    async def clear_purchased_clues(
+            self,
+            user_id: int,
+            task_global_id: int
+    ):
+        await self.session.execute(
+            delete(PurchasedClue).where(
+                (PurchasedClue.user_id == user_id) &
+                (PurchasedClue.task_global_id == task_global_id)
+            )
+        )
