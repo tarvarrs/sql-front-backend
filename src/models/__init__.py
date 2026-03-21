54aaaa29273981b744d136e8d72ea2ef114a85e0
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2026-03-21 15:30:07 +0300

    feat: add all models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,18 +1,25 @@
-# Регистрируем все модели для корректной работы SQLAlchemy
 from src.models.achievement import (
     Achievement,
     UserAchievementProgress,
     UsersAchievements,
 )
+from src.models.clue import PurchasedClue
 from src.models.progress import UserProgress
-from src.models.user import PasswordHash
-
-from .task import Task, TaskSolved
-from .user import User
+from src.models.quest import UserQuestProgress
+from src.models.task import Task, TaskSolved
+from src.models.user_event import UserEvent
+from src.models.user import User, PasswordHash
 
 __all__ = [
-    "User",
+    "Achievement",
+    "UserAchievementProgress",
+    "UsersAchievements",
+    "PurchasedClue",
+    "UserProgress",
+    "UserQuestProgress",
     "Task",
     "TaskSolved",
-    # ... остальные модели
+    "UserEvent",
+    "User",
+    "PasswordHash",
 ]
